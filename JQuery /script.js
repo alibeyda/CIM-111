6f7b99cf94d0f10ed487e342cb14e92064d3c575
Alison Beyda <alb276@miami.edu>
2018-03-28 13:21:50 -0400

    commit

---
+++ JQuery /script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $(".imageone").hide();
+    });
+
+    $("#show").click(function(){
+     $(".imageone").show();
+ });
+
+  });
