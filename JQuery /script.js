36d68a687823284047f6bc0e611e93ebfdb0a0e1
Alison Beyda <alb276@miami.edu>
2018-04-02 13:22:50 -0400

    commit

--- JQuery /script.js
+++ JQuery /script.js
@@ -5,6 +5,31 @@ $(document).ready(function(){
 
     $("#show").click(function(){
      $(".imageone").show();
- });
+   });
+
+   $(document).ready(function(){
+     $("#title").hover(function(){
+       $("#title").css("background-color", "black");
+       $("#title",).css("color", "white");
+   });
+   });
+
+ $("#clickhere").click(function(){
+    $("#replace").html("<p>For more information check out <a href='https://postmates.com'>postmates.com</a>");
+  });
+
+$(".change").hover(function(){
+    $("#only",).css("background-color", "red");
+    $("#only",).css("color", "white");
+  });
+
+  $(document).ready(function(){
+      $("#hide2").click(function(){
+          $(".imagetwo").hide();
+      });
+  $("#show2").click(function(){
+       $(".imagetwo").show();
+     });
+   });
 
   });
